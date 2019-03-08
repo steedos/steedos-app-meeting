af3df6b0f0f9b6bb0d578468d362e53d0083596c
Jack <zhuangjianguo@hotoa.com>
2019-03-08 23:27:04 +0800

    新 js 语法

---
+++ src/meeting.field.js
@@ -0,0 +1,28 @@
+exports.extendFieldClient = {
+  object: "meeting",
+  on: "client",
+  fields: {
+    room: {
+      options: function (values) {
+        var options, result, rooms;
+        result = [];
+        options = {
+          $orderby: 'name',
+          $select: 'enable_open,admins,name'
+        };
+        rooms = Creator.odata.query('meetingroom', options, true);
+        rooms.forEach(function (room) {
+          var ref;
+
+          if ((room != null ? (ref = room.admins) != null ? ref.indexOf(Meteor.userId()) : void 0 : void 0) > -1 || (room != null ? room.enable_open : void 0) || Steedos.isSpaceAdmin()) {
+            return result.push({
+              label: room.name,
+              value: room._id
+            });
+          }
+        });
+        return result;
+      }
+    }
+  }
+}
